4910db28f1872d9b55aa7e38b232e2a88230e614
Gondi László <laszlo.zsolt.gondi@gmail.com>
2022-10-11 14:55:19 +0200

    Make tests for all classes and the interface

--- src/hu/petrik/kolcsonzo/Main.java
+++ src/hu/petrik/kolcsonzo/Main.java
@@ -1,7 +1,23 @@
 package hu.petrik.kolcsonzo;
 
-public class Main {
-    public static void main(String[] args) {
+import java.util.List;
 
+public class Main {
+    static List<Kolcsonozheto> katalogus;
+
+    public static void main(String[] args) {
+        Konyv konyv1= new Konyv("Dune", "Frank Herbert", "AABBCC-112233");
+        Dvd dvd1 = new Dvd("Interstellar", 169);
+        Dvd dvd2 = new Dvd("Inception", 148);
+        Ujsag ujsag1= new Ujsag("Képes sport", 2007, 23);
+
+        katalogus.add(konyv1);
+        katalogus.add(dvd1);
+        katalogus.add(dvd2);
+        katalogus.add(ujsag1);
+
+        for (Kolcsonozheto k:katalogus) {
+            System.out.println(k.megjelenitendoNev()+" "+k.meddigKolcsonozoheto());
+        }
     }
 }
