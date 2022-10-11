e9e030e458c4071e4fe3e5d86616ea5932128f0c
Gondi László <laszlo.zsolt.gondi@gmail.com>
2022-10-11 14:38:35 +0200

    Override functions in Konyv class

--- src/hu/petrik/kolcsonzo/Konyv.java
+++ src/hu/petrik/kolcsonzo/Konyv.java
@@ -1,14 +1,23 @@
 package hu.petrik.kolcsonzo;
 
 public class Konyv implements Kolcsonozheto{
+    private String cim;
+    private String szerzo;
+    private String isbn;
+
+    public Konyv(String cim, String szerzo, String isbn) {
+        this.cim = cim;
+        this.szerzo = szerzo;
+        this.isbn = isbn;
+    }
 
     @Override
     public String megjelenitendoNev() {
-        return null;
+        return String.format("%s - %s",this.szerzo, this.cim);
     }
 
     @Override
     public int meddigKolcsonozoheto() {
-        return 0;
+        return 14;
     }
 }
